a4f383ad9e6878b783ce025faafd1656dd32b0de
toha <tha@tzi.de>
2013-11-15 02:07:33 +0000

    further improvements

---
+++ src/helper.c
@@ -0,0 +1,20 @@
+#include "helper.h"
+
+//assumes little endian
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = b[i] & (1<<j);
+            byte >>= j;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
---
+++ src/helper.h
@@ -0,0 +1,11 @@
+#ifndef _HELPER_H_
+#define _HELPER_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void printBits(size_t const size, void const * const ptr);
+
+
+#endif
